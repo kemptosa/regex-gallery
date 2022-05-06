1655b9d0942289616467374a35a9da4503898a2d
kemptosa <99417853+kemptosa@users.noreply.github.com>
2022-05-06 16:57:25 -0400

    first 2 levels working; some bugs probably

--- code.js
+++ code.js
@@ -103,6 +103,10 @@ let levelData = {
     statictargets:['regex'],
     dynamictargets:[],
     matchregex:'regex',
+    matchregexflags:'g',
+    checkgroups: false,
+    entries: 1,
+    hideflags: true,
     leveltext:'Welcome to Regex Gallery! Regex is an extremely powerful tool for searching and replacing within text. To start off, try matching the word "regex" as seen on the left. To start, enter a regular expression into the space in the bottom left between the <span class="code">//</span>.<br/><br/>The simplest way to match something with regex is to simply type it out. Most characters in regex have no explicit meaning on their own and can be used with no special considerations.',
     addref:[],
     next:'cats_and_bars',
@@ -114,40 +118,150 @@ let levelData = {
                     'bar', 'par', 'jar',
                     'bot', 'lot', 'jot', 'rot', 'cot'],
     matchregex:'.a.',
+    matchregexflags:'g',
+    checkgroups: false,
+    entries: 1,
+    hideflags: true,
     leveltext:'Here you will need the first special token, <span class="code">.</span><br/><br/>The <span class="code">.</span> token is a "wildcard"; it will match any character or symbol in a given position.<br/><br/>To complete this level, you should match any 3 letter word with an "a" in the center.',
     addref:[['.', 'wildcard']],
     next:'cats_and_caaaats',
     prev:'intro'
     }
 }
+let curLevelId = 'intro'
+let curLevel = null
+let curEntries = []
+let curTargets = []
+function spanifyAndCheck() {
+    let isAllComplete = true
+    for (const target of curTargets) {
+        let matchIndices = regexToIndices(curLevel.matchregex, curLevel.matchregexflags, target.innerText, curLevel.includeGroups)
+        let isComplete = curEntries.length > 0
+        let spans = []
+        for (const [regex, flags] of curEntries) {
+            let indices = regexToIndices(regex.innerText, flags.innerText, target.innerText, curLevel.checkgroups)
+            isComplete = isComplete && (JSON.stringify(indices) === JSON.stringify(matchIndices))
+            spans.push(...indicesToSpans(indices, 'at'))
+        }
+        spans.push(...indicesToSpans(matchIndices, 'ht'))
+        target.innerHTML = overlapSpan(target.innerText, spans)
+        if (isComplete) {
+            target.classList.add('died')
+        } else {
+            target.classList.remove('died')
+        }
+        isAllComplete = isAllComplete && isComplete
+    }
+    if (isAllComplete) {
+        gameData.completed.push(curLevelId)
+        game.classList.add('fadeout')
+        setTimeout(()=>{
+            game.classList.remove('fadeout')
+            start(curLevel.next)
+        }, 1000)
+    }
+}
+function indicesToSpans(indices, className) {
+    let result = []
+    for (const outer of indices) {
+        for (const inner of outer) {
+            result.push([className, ...inner])
+        }
+    }
+    return result
+}
 function start(level) {
-    let startLevel = 'intro'
     if (level === undefined) {
-        while (levelData[startLevel].next !== null) {
-            let next = levelData[startLevel].next
-            if (gameData.completed.includes(startLevel)) {
-                startLevel = next
+        while (levelData[curLevelId].next !== null) {
+            let next = levelData[curLevelId].next
+            if (gameData.completed.includes(curLevelId)) {
+                curLevelId = next
             } else {
                 break
             }
         }
     } else {
-        startLevel = level
+        curLevelId = level
     }
-    rightCol.querySelector('.leveltext').innerHTML = levelData[startLevel].leveltext
+    curLevel = levelData[curLevelId]
+    
+    rightCol.querySelector('.leveltext').innerHTML = curLevel.leveltext
     rightCol.classList.remove('closed')
     bottomPane.classList.remove('closed')
-    for ([index, target] of levelData[startLevel].dynamictargets.sort(()=>Math.random()-0.5).entries()) {
+    game.innerHTML = ''
+    regexEntry.innerHTML = ''
+    subEntry.innerHTML = ''
+    for (const [index, target] of curLevel.dynamictargets.sort(()=>Math.random()-0.5).entries()) {
         newTarget = document.createElement('p')
         newTarget.className = 'target scroller'
         newTarget.style = `top: ${index}em; animation-delay: -${Math.random() * 30}s;`
         newTarget.innerText = target
+        curTargets.push(newTarget)
         game.append(newTarget)
     }
-    for (target of levelData[startLevel].statictargets) {
+    for (const target of curLevel.statictargets) {
         newTarget = document.createElement('p')
         newTarget.className = 'target'
         newTarget.innerText = target
+        curTargets.push(newTarget)
         game.append(newTarget)
     }
+    curEntries = []
+    spanifyAndCheck()
+    for (let i = 0; i < curLevel.entries; i += 1) {
+        let newRegexInput = document.createElement('span')
+        let newFlagInput = document.createElement('span')
+        let newP = document.createElement('p')
+        newRegexInput.className = 'regex-input'
+        newRegexInput.contentEditable = true
+        newFlagInput.className = 'flag-input'
+        newFlagInput.contentEditable = true
+        newP.className = 'regex-entry'
+        newP.append('/', newRegexInput, '/')
+        if (curLevelId === 'intro') {
+            newRegexInput.classList.add('highlight')
+        }
+        if (!curLevel.hideflags) {
+            newP.append(newFlagInput)
+        }
+        newRegexInput.addEventListener('focusout', fixText)
+        newFlagInput.addEventListener('focusout', fixText)
+        curEntries.push([newRegexInput, newFlagInput])
+        regexEntry.append(newP)
+    }
 }
+function fixText() {
+    this.innerText = this.innerText
+}
+function regexToIndices(regexString, regexFlags, matchString, includeGroups) {
+    regexFlags = regexFlags.toLowerCase()
+    if (!regexFlags) {
+        regexFlags = ""
+    }
+    if (!regexFlags.includes('d')) {
+        regexFlags += 'd'
+    }
+    if (!regexFlags.includes('g') && curLevel.hideflags) {
+        regexFlags += 'g'
+    }
+    let indices = []
+    let regex = new RegExp(regexString, regexFlags)
+    do {
+        indices.push(regex.exec(matchString))
+    } while (regex.lastIndex !== 0)
+    indices.pop()
+    if (!includeGroups) {
+        return indices.map(i=>[[...i.indices[0]]])
+    } else {
+        return indices.map(i=>[...i.indices])
+    }
+    
+}
+function handleEntry(e) {
+    if (e.keyCode === 13) {
+        e.preventDefault()
+        document.activeElement.blur()
+        spanifyAndCheck()
+    }
+}
+document.addEventListener('keyup', handleEntry)
